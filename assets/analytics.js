151e84300cd766a4dd61ca27f4e9b78da5aa6ce8
Hakaru Hirose <hirose@hakaru.net>
2026-01-26 11:36:27 +0900

    refactor: Centralize GA4 tracking in assets/analytics.js

---
+++ assets/analytics.js
@@ -0,0 +1,18 @@
+// MIDI2Kit - Google Analytics 4 Configuration
+// Measurement ID: G-GQBDG41DZ7
+
+(function() {
+    // Load gtag.js
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-GQBDG41DZ7';
+    document.head.appendChild(script);
+
+    // Initialize dataLayer and gtag
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    
+    gtag('js', new Date());
+    gtag('config', 'G-GQBDG41DZ7');
+})();
